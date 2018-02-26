2731b094e2eea0e1c42548c585b6e64ac2be0c04
WillemsBrecht <brecht.willems2@student.howest.be>
2018-02-26 11:42:56 +0100

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewShoppingApp.Models
+{
+    public class Class
+    {
+        public int Year { get; set; }
+        public int students { get; set; }
+        public string Name { get; set; }
+    }
+}
